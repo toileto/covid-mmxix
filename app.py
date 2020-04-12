bd819d2c95542068e794c8b4583ccff3b0506690
atnanahidiw <atnanahidiw@gmail.com>
2020-04-12 19:31:42 +0700

    bugfix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from src.model.visualizer import visualize_province_table, visualize_overview
 
 
 STATUS = ["positif", "sembuh", "meninggal"]
-WAKTU_JALAN_STR = WAKTU_JALAN_STR[:16]
+WAKTU_JALAN_HH = WAKTU_JALAN_STR[:16]
 
 def write_readme(namafile):
     with open(namafile, "w") as f:
@@ -12,7 +12,7 @@ def write_readme(namafile):
         f.write(f"Dashboard untuk menampilkan tingkat keseragaman data statistik kasus COVID-19 di Indonesia antara pusat & daerah. \\\n")
         f.write(f"[Klik tautan ini untuk informasi lebih lanjut](INFO.md)\n")
         f.write("\n<br>\n\n")
-        f.write(f"#### Update: {WAKTU_JALAN_STR} WIB\n")
+        f.write(f"#### Update: {WAKTU_JALAN_HH} WIB\n")
         f.write(f"## Selisih Pusat vs Daerah\n")
         f.write(f"Sumber Pusat: {parser_pusat.DATA_URL}\n")
         f.write(tabel_data_provinsi)
@@ -39,7 +39,7 @@ for k in data_semua_provinsi.keys():
                 "sumber": prov_parser.WEB_URL
             })
         except Exception as e:
-            print(e)
+            print(k, e)
             data_semua_provinsi[k].update({
                 "error": True,
                 "sumber": prov_parser.WEB_URL
@@ -57,7 +57,7 @@ summary_data = {
 # write to file
 write_readme(f"README.md")
 
-namafile_ts = WAKTU_JALAN_STR.replace(' ', 'T')
+namafile_ts = WAKTU_JALAN_HH.replace(' ', 'T')
 write_readme(f"data/historical/markdown/README_{namafile_ts}.md")
 with open(f"data/historical/json/{namafile_ts}.json", "w") as f:
     json.dump(summary_data, f)
